3d5ed8e05caee4d442c6db8e20e5cd757a87ae34
cleme <clementc2013@gmail.com>
2023-11-30 15:33:31 +0100

    Still not working

--- actions/Q7.py
+++ actions/Q7.py
@@ -19,47 +19,46 @@ class Window(tk.Toplevel):
         ttk.Label(self, text='Selectionnez une action : ', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=0, column=3)
         self.input = ttk.Combobox(self, values=["Ajout","Modifier","Suprimer"], state="readonly")
         self.input.current(0)
-        self.input.bind('<<ComboboxSelected>>', self.select)
-        self.input.bind('<Return>', self.select)
+        #self.input.bind('<<ComboboxSelected>>', self.select)
         self.input.grid(row=0, column=4)
-        
+
+        ttk.Label(self, text='Veuillez indiquer le type de travaux :', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=1, column=3)    
+        choix=["Photovoltaïque","Chauffage","Isolation"] 
+        self.input2 = ttk.Combobox(self, values=choix, state="readonly")
+        self.input2.current(0)
+        self.input2.grid(row=1, column=4)
+        self.input2.bind('<<ComboboxSelected>>', self.select)
+
     def select(self, event):
+        print(self.input.get())
         if self.input.get() == "Ajout":
-            self.ajoutMain()
+            self.ajout(self.input2)
         elif self.input.get() == "Modifier":
-            self.modifierMain()
+            self.modifier
         elif self.input.get() == "Suprimer":
-            self.suprimerMain()
+            self.suprimer
             
-    def ajoutMain(self):
+    # def ajoutMain(self):
         
-        ttk.Label(self, text='Veuillez indiquer le type de travaux :', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=1, column=3)    
-        choix=["Photovoltaïque","Chauffage","Isolation"] 
-        self.input = ttk.Combobox(self, values=choix, state="readonly")
-        self.input.current(0)
-        self.input.bind('<<ComboboxSelected>>', self.ajout)
-        self.input.grid(row=1, column=4)
+    #     ttk.Label(self, text='Veuillez indiquer le type de travaux :', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=1, column=3)    
+    #     choix=["Photovoltaïque","Chauffage","Isolation"] 
+    #     self.input = ttk.Combobox(self, values=choix, state="readonly")
+    #     self.input.current(0)
+    #     self.input.bind('<<ComboboxSelected>>', self.ajout)
+    #     
     
-    def modifierMain(self):
-        ttk.Label(self, text='Veuillez indiquer le type de travaux :', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=1, column=3)    
-        choix=["Photovoltaïque","Chauffage","Isolation"] 
-        self.input = ttk.Combobox(self, values=choix, state="readonly")
-        self.input.current(0)
-        self.input.bind('<<ComboboxSelected>>', self.modifier)
-        self.input.grid(row=1, column=4)
         
-    def suprimerMain(self):
-        ttk.Label(self, text='Veuillez indiquer le type de travaux :', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=1, column=3)    
-        choix=["Photovoltaïque","Chauffage","Isolation"] 
-        self.input = ttk.Combobox(self, values=choix, state="readonly")
-        self.input.current(0)
-        self.input.bind('<<ComboboxSelected>>', self.suprimer)
-        self.input.grid(row=1, column=4)
         
     def ajout(self, event):
+        bout=None
         for i in labels:
-            i.grid_remove()
-        labels.clear()                  
+            print(i)
+            if i:
+                i.destroy()
+        if bout:
+            bout.destroy()
+        labels.clear()
+        print(event.get())                  
         ttk.Label(self, text='Veuillez indiquer les valeurs à ajouter :', anchor="center", font=('Helvetica', '10', 'bold')).grid(row=2, column=3, columnspan=8)
         ttk.Label(self, text='id_travaux:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=0)
         self.id_travaux_entry = ttk.Label(self,text="auto")
@@ -72,44 +71,44 @@ class Window(tk.Toplevel):
         labels.append(self.cout_total_ht_entry)
 
 
-        ttk.Label(self, text='cout_induit_ht:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=3)
+        ttk.Label(self, text='cout_induit_ht:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=2)
         self.cout_induit_ht_entry = ttk.Entry(self)
-        self.cout_induit_ht_entry.grid(row=4, column=3)
+        self.cout_induit_ht_entry.grid(row=4, column=2)
         labels.append(self.cout_induit_ht_entry)
 
-        ttk.Label(self, text='annee_travaux:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=4)
+        ttk.Label(self, text='annee_travaux:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=3)
         self.annee_travaux_entry = ttk.Entry(self)
-        self.annee_travaux_entry.grid(row=4, column=4)
+        self.annee_travaux_entry.grid(row=4, column=3)
         labels.append(self.annee_travaux_entry)
 
-        ttk.Label(self, text='code_departement:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=5)
+        ttk.Label(self, text='code_departement:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=4)
         self.code_departement_entry = ttk.Entry(self)
-        self.code_departement_entry.grid(row=4, column=5)
+        self.code_departement_entry.grid(row=4, column=4)
         labels.append(self.code_departement_entry)
 
-        ttk.Label(self, text='code_region:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=6)
+        ttk.Label(self, text='code_region:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=5)
         self.code_region_entry = ttk.Entry(self)
-        self.code_region_entry.grid(row=4, column=6)
+        self.code_region_entry.grid(row=4, column=5)
         labels.append(self.code_region_entry)
 
-        ttk.Label(self, text='type_logement:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=7)
+        ttk.Label(self, text='type_logement:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=8)
         self.type_logement_entry = ttk.Entry(self)
-        self.type_logement_entry.grid(row=4, column=7)
+        self.type_logement_entry.grid(row=4, column=8)
         labels.append(self.type_logement_entry)
 
-        ttk.Label(self, text='anne_construction_logement:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=8)
+        ttk.Label(self, text='anne_construction_logement:', anchor="center", font=('Helvetica', '10')).grid(row=3, column=9)
         self.anne_construction_logement_entry = ttk.Entry(self)
-        self.anne_construction_logement_entry.grid(row=4, column=8)
+        self.anne_construction_logement_entry.grid(row=4, column=9)
         labels.append(self.anne_construction_logement_entry)
     
-        if(event.widget.get()=="Photovoltaïque"):
+        if(event.get()=="Photovoltaïque"):
             #('puissance_installee','type_panneaux')
-            ttk.Label(self, text='puissance_installee:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0)
+            labels.append(ttk.Label(self, text='puissance_installee:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0))
             self.puissance_installee_entry = ttk.Entry(self)
             self.puissance_installee_entry.grid(row=6, column=0)
             labels.append(self.puissance_installee_entry)
 
-            ttk.Label(self, text='type_panneaux:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=1)
+            labels.append(ttk.Label(self, text='type_panneaux:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=1))
             self.type_panneaux_entry = ttk.Combobox(self, values=["Polycristallin","Monocristallin"], state="readonly")
             self.type_panneaux_entry.grid(row=6, column=1, columnspan=2)
             labels.append(self.type_panneaux_entry)
@@ -117,24 +116,24 @@ class Window(tk.Toplevel):
             bout = ttk.Button(self, text='Ajouter', command=self.ajoutPhotovoltaique).grid(row=7, column=0)
             #labels.append(bout)
     
-        elif(event.widget.get()=="Chauffage"):
+        elif(event.get()=="Chauffage"):
             #('énergie_avant_travaux','energie_installee','generateur','type_chaudiere')
-            ttk.Label(self, text='énergie_avant_travaux:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0)
+            labels.append(ttk.Label(self, text='énergie_avant_travaux:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0))
             self.energie_avant_travaux_entry = ttk.Entry(self)
             self.energie_avant_travaux_entry.grid(row=6, column=0)
             labels.append(self.energie_avant_travaux_entry)
             
-            ttk.Label(self, text='energie_installee:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=1)
+            labels.append(ttk.Label(self, text='energie_installee:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=1))
             self.energie_installee_entry = ttk.Entry(self)
             self.energie_installee_entry.grid(row=6, column=1)
             labels.append(self.energie_installee_entry)
             
-            ttk.Label(self, text='generateur:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=2)
+            labels.append(ttk.Label(self, text='generateur:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=2))
             self.generateur_entry = ttk.Entry(self)
             self.generateur_entry.grid(row=6, column=2)
             labels.append(self.generateur_entry)
             
-            ttk.Label(self, text='type_chaudiere:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=3)
+            labels.append(ttk.Label(self, text='type_chaudiere:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=3))
             self.type_chaudiere_entry = ttk.Entry(self)
             self.type_chaudiere_entry.grid(row=6, column=3)
             labels.append(self.type_chaudiere_entry)
@@ -142,19 +141,19 @@ class Window(tk.Toplevel):
             bout=ttk.Button(self, text='Ajouter', command=self.ajoutChauffage).grid(row=7, column=0)
             #labels.append(bout)
         
-        elif(event.widget.get()=="Isolation"):
+        elif(event.get()=="Isolation"):
             #('poste','type_isolant','epaisseur_isolant','surface')
-            ttk.Label(self, text='poste_isolation:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0)
+            labels.append(ttk.Label(self, text='poste_isolation:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0))
             self.poste_entry = ttk.Entry(self)
             self.poste_entry.grid(row=6, column=0)
             labels.append(self.poste_entry)
             
-            ttk.Label(self, text='type_isolant:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=1)
+            labels.append(ttk.Label(self, text='type_isolant:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=1))
             self.type_isolant_entry = ttk.Entry(self)
             self.type_isolant_entry.grid(row=6, column=1)
             labels.append(self.type_isolant_entry)
             
-            ttk.Label(self, text='epaisseur:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0)
+            labels.append(ttk.Label(self, text='epaisseur:', anchor="center", font=('Helvetica', '10')).grid(row=5, column=0))
             self.epaisseur_isolant_entry = ttk.Entry(self)
             self.epaisseur_isolant_entry.grid(row=6, column=0)
             labels.append(self.epaisseur_isolant_entry)
@@ -164,7 +163,7 @@ class Window(tk.Toplevel):
          # On place un label sans texte, il servira à afficher les erreurs
         self.errorLabel = ttk.Label(self, anchor="center", font=('Helvetica', '10', 'bold'))
         self.errorLabel.grid(columnspan=3, row=12, sticky="we")
-        labels.append(self.errorLabel)
+        
     
     def modifier(self,event):
         for i in labels:
